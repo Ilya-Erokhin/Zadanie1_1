689cb063ef3c8868cc5559678c585f975be9450a
Ilya01 <illyaerohin@yandex.ru>
2021-03-11 21:52:22 +0300

    Files Was Added

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean each20 = true;
+        int RUB;
+        if(each20) {
+            RUB = 20;
+        } else {
+            RUB = 0;
+        }
+
+        RUB = 20000;
+        int RUB1 = 20;
+
+        int mile2 = RUB / RUB1;
+        System.out.println(mile2);
+    }
+}
